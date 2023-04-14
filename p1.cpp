7543021f328ff5c80e1cc2ca3eae6670fe6f31af
Noam Yaffe <noamyaffe19@gmail.com>
2023-04-14 02:21:42 +0100

    p1

--- p1.cpp
+++ p1.cpp
@@ -8,7 +8,7 @@ int main() {
   int numOfItems;
   int counter;
   int sum = 0;
-  
+
   cin >> numOfSets;
 
   int output[numOfSets];
@@ -17,37 +17,30 @@ int main() {
 
     counter = 0;
     cin >> numOfItems;
-    char input[numOfItems];
-    cin >> input;
-    cin.get();
     int arr[numOfItems];
-
     for (int i = 0; i < numOfItems; i++) {
-	if (input[i] == ' ') {
-	  continue;
-	}
-	arr[i] = (int)(input[i]) - 48;
-    }
-    
-    for (int a = 0; a < numOfItems; a++) {
-      
-      for (int b = a + 1; b < numOfItems; b++) {
-	sum = arr[a] + arr[b];
-	if (sum % 3 == 0) {
-	  counter++;
-	}
-      }
-      
+      cin >> arr[i];
     }
 
-	   counter = output[i];
-	   
+    for (int a = 0; a < numOfItems; a++) {
+
+      for (int b = a + 1; b < numOfItems; b++) {
+    sum = arr[a] + arr[b];
+    if (sum % 3 == 0) {
+      counter++;
+    }
+      }
+
+    }
+
+    output[i] = counter;
+    
   }
 
-    for (int i = 0; i < numOfSets; i++) {
-      cout << output[i] << endl;
-    }
+  for (int i = 0; i < numOfSets; i++) {
+    cout << output[i] << endl;
+  }
 
   return 0;
-  
+
 }
