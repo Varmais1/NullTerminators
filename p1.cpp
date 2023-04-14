57d4d75947f06d292558cd77976c11eefae5749d
Noam Yaffe <noamyaffe19@gmail.com>
2023-04-14 01:34:35 +0100

    p1

---
+++ p1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int numOfSets = 0;
+  int numOfItems;
+  int counter;
+  int sum = 0;
+  
+  cin >> numOfSets;
+
+  for (int i = 0; i < numOfSets; i++) {
+
+    counter = 0;
+    cin >> numOfItems;
+    int arr[numOfItems];
+    for (int i = 0; i < numOfItems; i++) {
+      cin >> arr[i];
+    }
+    
+    for (int a = 0; a < numOfItems; a++) {
+      
+      for (int b = a + 1; b < numOfItems; b++) {
+	sum = arr[a] + arr[b];
+	if (sum % 3 == 0) {
+	  counter++;
+	}
+      }
+      
+    }
+    
+    cout << counter << endl;
+
+  }
+  
+}
